0921a90340e8979ca24e37021f266265aa534f45
Sachin RS <43383547+sach999@users.noreply.github.com>
2019-12-24 17:38:37 +0530

    Add files via upload

---
+++ stroke-draw/app.js
@@ -0,0 +1,70 @@
+window.onload = function () {
+
+    document.ontouchmove = function (e) { e.preventDefault(); }
+
+    var canvas = document.getElementById('main');
+    var canvastop = canvas.offsetTop
+
+    var context = canvas.getContext("2d");
+
+    let h = canvas.height = window.innerHeight;
+    let w = canvas.width = window.innerWidth;
+
+    var lastx;
+    var lasty;
+
+    context.strokeStyle = "#000000";
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+    context.lineWidth = 5;
+
+    function clear() {
+        context.fillStyle = "#ffffff";
+        context.rect(0, 0, w, h);
+        context.fill();
+    }
+
+    function dot(x, y) {
+        context.beginPath();
+        context.fillStyle = "#000000";
+        context.arc(x, y, 1, 0, Math.PI * 2, true);
+        context.fill();
+        context.stroke();
+        context.closePath();
+    }
+
+    function line(fromx, fromy, tox, toy) {
+        context.beginPath();
+        context.moveTo(fromx, fromy);
+        context.lineTo(tox, toy);
+        context.stroke();
+        context.closePath();
+    }
+
+    canvas.ontouchstart = function (event) {
+        event.preventDefault();
+
+        lastx = event.touches[0].clientX;
+        lasty = event.touches[0].clientY - canvastop;
+
+        dot(lastx, lasty);
+    }
+
+    canvas.ontouchmove = function (event) {
+        event.preventDefault();
+
+        var newx = event.touches[0].clientX;
+        var newy = event.touches[0].clientY - canvastop;
+
+        line(lastx, lasty, newx, newy);
+
+        lastx = newx;
+        lasty = newy;
+    }
+
+
+    var clearButton = document.getElementById('clear')
+    clearButton.onclick = clear
+
+    clear()
+}
